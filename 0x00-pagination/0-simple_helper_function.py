433b6d23652cddf8111c761d1ea27a963112bc2e
kelechi-aims <kelechi@aims.edu.gh>
2024-01-26 12:13:28 +0100

    function that calculates the start and end indexes for pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 """ Simple helper function that calculates the start and
 end indexes for pagination
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calulate the start and end indexes for pagination
 
