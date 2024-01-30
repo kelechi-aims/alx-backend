2f7b502b4747594055281cc137bd9d99900460e5
kelechi-aims <kelechi@aims.edu.gh>
2024-01-30 18:54:09 +0100

    readme upload

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,5 +20,7 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ Gets an item by key """
-        if key is not None or key not in self.cache_data:
+        if key is None or key not in self.cache_data:
+            return None
+        else:
             return self.cache_data[key]
