1812c35f0c0dc35d2bfe27c67ea7b5de558336ed
kelechi-aims <kelechi@aims.edu.gh>
2024-02-07 01:00:37 +0100

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+""" Mock logging in """
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+# Define user table
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """ Supported languages list."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Use the Config class as the configuration for the Flask app
+app.config.from_object(Config)
+
+
+# Define supported locales using request.accept_languages
+@babel.localeselector
+def get_locale():
+    """ Check if locale parameter is present in the request URL """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# Define get_user function to get user details based on user ID
+def get_user(user_id):
+    """ Get user details based on user ID """
+    return users.get(user_id)
+
+# Define before_request function to execute before all other functions
+@app.before_request
+def before_request():
+    """ Check if login_as parameter is present in the request URL """
+    user_id = request.args.get('login_as')
+    if user_id:
+        # Attempt to get user details based on user ID
+        user = get_user(int(user_id))
+        if user:
+            # Set user details as a global on flask.g.user
+            g.user = user
+        else:
+            # If user ID not found, set g.user to None
+            g.user = None
+    else:
+        # If login_as parameter not present, set g.user to None
+        g.user = None
+
+
+@app.route('/')
+def index():
+    """ rendering index html """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
